ec6b5264c7c6bd81485172dcd52fa3cd3bca66fc
Zhuoheng Xie <zh-xie@live.com>
2021-06-21 16:18:25 +0800

    update rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, enableProdMode } from '@angular/core';
+
+import {
+  Service,
+  Task,
+  Dependency,
+  Resource,
+  ResourceAssignment,
+} from './app.service';
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [Service],
+  preserveWhitespaces: true,
 })
 export class AppComponent {
-  title = 'test-gantt';
+  tasks: Task[];
+  dependencies: Dependency[];
+  resources: Resource[];
+  resourceAssignments: ResourceAssignment[];
+  scaleType: string;
+  titlePosition: string;
+  showResources: boolean;
+  taskTooltipContentTemplate: (model: any) => string;
+
+  constructor(service: Service) {
+    this.tasks = service.getTasks();
+    this.dependencies = service.getDependencies();
+    this.resources = service.getResources();
+    this.resourceAssignments = service.getResourceAssignments();
+    this.scaleType = 'quarters';
+    this.titlePosition = 'outside';
+    this.showResources = true;
+    this.taskTooltipContentTemplate = this.getTaskTooltipContentTemplate;
+  }
+
+  getTaskTooltipContentTemplate(model) {
+    const timeEstimate = Math.abs(model.start - model.end) / 36e5;
+    const timeLeft = Math.floor(((100 - model.progress) / 100) * timeEstimate);
+
+    return (
+      `<div class="template-header"> ${model.title} </div>` +
+      `<p class="template-item"> <span> Estimate: </span> ${timeEstimate} <span> hours </span> </p>` +
+      `<p class="template-item"> <span> Left: </span> ${timeLeft} <span> hours </span> </p>`
+    );
+  }
+
+  onShowCustomTaskTooltip(e) {
+    e.value
+      ? (this.taskTooltipContentTemplate = this.getTaskTooltipContentTemplate)
+      : (this.taskTooltipContentTemplate = undefined);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DxGanttModule, DxCheckBoxModule, DxSelectBoxModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxGanttModule,
+    DxCheckBoxModule,
+    DxSelectBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
